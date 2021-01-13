2fc522b59bf01d7abea85dd51f8af69f2615cc10
Akshit Sadana <akshitsadana@gmail.com>
2021-01-13 23:58:14 +0530

    config package started

---
+++ src/config/config.go
@@ -0,0 +1,10 @@
+/*
+ * @File: config.config.go
+ * @Description: Defines configuration of the service
+ * @LastModifiedTime: 2021-01-13 18:24:32 
+ * @Author: Akshit Sadana (akshitsadana@gmail.com) 
+ */
+
+// Package config defines service configuration
+package config
+
